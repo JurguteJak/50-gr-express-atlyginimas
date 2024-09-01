2d8db6dc4ffdc7c88a487bc912ceda108ecb28d2
JurguteJak <jurgutejak@gmail.com>
2024-09-01 11:59:18 +0300

    commit

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import { pageHome } from './pages/pageHome.js';
+import { page404 } from './pages/page404.js';
+import { pageAbout } from './pages/pageAbout.js';
 
 const app = express();
 const port = 5036;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    return res.send('STRING');
+    return res.send(pageHome());
+})
+
+app.get('/about', (req, res) => {
+    return res.send(pageAbout());
+})
+
+// app.get('/css/main.css', (req, res) => {
+//     return res.send(pageAbout());
+// })
+
+app.get('*', (req, res) => {
+    return res.send(page404());
 })
 
 app.listen(port, () => {
