22539ebd8668003620a7318ccceae5e8acdcbcfe
JurguteJak <jurgutejak@gmail.com>
2024-09-02 10:46:52 +0300

    midlle

--- pages/pageCreateAccount.js
+++ pages/pageCreateAccount.js
@@ -1,14 +1,21 @@
 import { footer } from "../components/footer.js";
 import { head } from "../components/head.js";
 import { header } from "../components/header.js";
+import { accountsData } from "../data/accountsData.js";
 
 export function pageCreateAccount() {
     const date = new Date();
     const y = date.getFullYear();
     const m = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
     const d = (date.getDate() < 10 ? '0' : '') + date.getDate();
-
     const maxDate = `${y}-${m}-${d}`;
+
+    accountsData.push({
+        name: 'Petras',
+        date: '2023-03-14',
+        rate: 2.73,
+        salary: '---',
+    });
 
     return `
         <!DOCTYPE html>
